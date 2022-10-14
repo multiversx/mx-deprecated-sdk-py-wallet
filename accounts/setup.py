ff5d5a5b7eb83117373ebeaa36e187621c47e52c
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-10-14 23:58:35 +0300

    Trial and error.

---
+++ accounts/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="erdpy-accounts",
+    version="0.1.0",
+    url="https://github.com/ElrondNetwork/elrond-sdk-erdpy-eggs",
+    author="Elrond Network",
+    license="MIT",
+    include_package_data=True,
+    setup_requires=["wheel"],
+    install_requires=[
+    ],
+    zip_safe=False,
+    keywords=["Elrond"],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers"
+    ],
+    python_requires=">=3.8"
+)

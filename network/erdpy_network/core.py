8b9a7fc26959597ae71e9d69b970f38034a6684a
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-11-08 13:06:37 +0200

    Fix references in network egg.

--- network/erdpy_network/core.py
+++ network/erdpy_network/core.py
@@ -1,15 +1,13 @@
 from typing import Any, Dict
 
 import requests
-from erdpy import errors
-from erdpy.proxy.messages import GenericProxyResponse
 
 
 class ProxyNetworkProvider():
     def __init__(self, url: str):
         self.url = url
 
-    def do_get(self, url: str) -> GenericProxyResponse:
+    def do_get(self, url: str) -> 'GenericResponse':
         try:
             response = requests.get(url)
             response.raise_for_status()
@@ -17,13 +15,13 @@ class ProxyNetworkProvider():
             return _get_data(parsed, url)
         except requests.HTTPError as err:
             error_data = _extract_error_from_response(err.response)
-            raise errors.ProxyRequestError(url, error_data)
+            raise GenericError(url, error_data)
         except requests.ConnectionError as err:
-            raise errors.ProxyRequestError(url, err)
+            raise GenericError(url, err)
         except Exception as err:
-            raise errors.ProxyRequestError(url, err)
+            raise GenericError(url, err)
 
-    def do_post(self, url: str, payload: Any) -> GenericProxyResponse:
+    def do_post(self, url: str, payload: Any) -> 'GenericResponse':
         try:
             response = requests.post(url, json=payload)
             response.raise_for_status()
@@ -31,22 +29,22 @@ class ProxyNetworkProvider():
             return _get_data(parsed, url)
         except requests.HTTPError as err:
             error_data = _extract_error_from_response(err.response)
-            raise errors.ProxyRequestError(url, error_data)
+            raise GenericError(url, error_data)
         except requests.ConnectionError as err:
-            raise errors.ProxyRequestError(url, err)
+            raise GenericError(url, err)
         except Exception as err:
-            raise errors.ProxyRequestError(url, err)
+            raise GenericError(url, err)
 
 
-def _get_data(parsed: Dict[str, Any], url: str) -> GenericProxyResponse:
+def _get_data(parsed: Dict[str, Any], url: str) -> 'GenericResponse':
     err = parsed.get("error")
     code = parsed.get("code")
 
     if not err and code == "successful":
         data: Dict[str, Any] = parsed.get("data", dict())
-        return GenericProxyResponse(data)
+        return GenericResponse(data)
 
-    raise errors.ProxyRequestError(url, f"code:{code}, error: {err}")
+    raise GenericError(url, f"code:{code}, error: {err}")
 
 
 def _extract_error_from_response(response: Any):
@@ -54,3 +52,19 @@ def _extract_error_from_response(response: Any):
         return response.json()
     except Exception:
         return response.text
+
+
+class GenericResponse():
+    def __init__(self, data: Any) -> None:
+        self.__dict__.update(data)
+
+    def get(self, key: str, default: Any = None) -> Any:
+        return self.__dict__.get(key, default)
+
+    def to_dictionary(self) -> Dict[str, Any]:
+        return self.__dict__
+
+
+class GenericError(Exception):
+    def __init__(self, url: str, data: Any):
+        super().__init__(f"Url = [{url}], error = {data}")

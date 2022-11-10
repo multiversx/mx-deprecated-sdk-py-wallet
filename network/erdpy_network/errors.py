1df4512424a50e0f43e1311f68670d127dd7815e
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-11-10 21:10:12 +0200

    Add error data.

--- network/erdpy_network/errors.py
+++ network/erdpy_network/errors.py
@@ -4,3 +4,5 @@ from typing import Any
 class GenericError(Exception):
     def __init__(self, url: str, data: Any):
         super().__init__(f"Url = [{url}], error = {data}")
+        self.url = url
+        self.data = data

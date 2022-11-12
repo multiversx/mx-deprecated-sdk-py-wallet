7c42e25f64ca190f3ff0a78a1b779c81e94574a9
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-11-12 20:09:51 +0200

    Annotate return.

--- wallet/erdpy_wallet/facade.py
+++ wallet/erdpy_wallet/facade.py
@@ -5,7 +5,7 @@ from erdpy_wallet import pem
 from erdpy_wallet.generator import generate_pair
 
 
-def generate_pem_file(pem_file: Path):
+def generate_pem_file(pem_file: Path) -> None:
     secret_key, pubkey = generate_pair()
     address = Address(pubkey)
     pem.write(pem_file, secret_key, pubkey, name=address.bech32())
